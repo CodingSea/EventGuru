770d2a7071dbfc8c09094507378b53438accf48a
CodingSea <fhdq2003@gmail.com>
2024-11-02 18:57:28 +0300

    Initial Commit

---
+++ EventGuruTests/EventGuruTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventGuruTests.swift
+//  EventGuruTests
+//
+//  Created by Fahad on 02/11/2024.
+//
+
+import Testing
+@testable import EventGuru
+
+struct EventGuruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
