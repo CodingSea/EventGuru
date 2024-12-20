5db2451cb12c23fe6cd94b1275ea06339c7f29ce
Ali <aj.299@hotmail.co.uk>
2024-12-20 17:42:51 +0300

    updated

---
+++ EventGuru02/Ali/HistoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryCollectionViewCell.swift
+//  EventGuru02
+//
+//  Created by Ali Juma on 14/12/2024.
+//
+
+import UIKit
+
+class HistoryCollectionViewCell: UICollectionViewCell {
+    
+}
