cabaced301239fd36aa906d142e48173023c9b27
Ali <aj.299@hotmail.co.uk>
2024-12-27 23:25:40 +0300

    updated

---
+++ EventGuru02/Ali/ViewControllerr.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerr.swift
+//  EventGuru02
+//
+//  Created by Ali Juma on 27/12/2024.
+//
+
+import UIKit
+
+class ViewControllerr: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
