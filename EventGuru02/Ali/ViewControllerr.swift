aaf13e96b77ce3344c56827b284b1e37273734aa
Ali <aj.299@hotmail.co.uk>
2024-12-28 21:07:26 +0300

    Buttons Working

--- EventGuru02/Ali/ViewControllerr.swift
+++ EventGuru02/Ali/ViewControllerr.swift
@@ -1,29 +1,75 @@
-//
-//  ViewControllerr.swift
-//  EventGuru02
-//
-//  Created by Ali Juma on 28/12/2024.
-//
-
 import UIKit
 
 class ViewControllerr: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Buy Ticket Action
+    @IBAction func BuyTicket(_ sender: Any) {
+        let alertController = UIAlertController(
+            title: "Ticket Purchase",
+            message: "Are you sure you want to buy this ticket?",
+            preferredStyle: .alert
+        )
+        
+        let confirmAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            print("Ticket purchased!")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("Purchase cancelled.")
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
-    */
 
+    // Cancel Reservation Action
+    @IBAction func CancelReservation(_ sender: Any) {
+        let alertController = UIAlertController(
+            title: "Cancel Reservation",
+            message: "Are you sure you want to cancel this ticket?",
+            preferredStyle: .alert
+        )
+        
+        let confirmAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            print("Ticket canceled!")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("Cancel reservation cancelled.")
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // Notify User Action
+    @IBAction func Notify(_ sender: Any) {
+        let alertController = UIAlertController(
+            title: "Notify if Available",
+            message: "Are you sure you want an Email to be sent once tickets are available?",
+            preferredStyle: .alert
+        )
+        
+        let confirmAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            print("Email will be sent!")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("Email notification cancelled.")
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
