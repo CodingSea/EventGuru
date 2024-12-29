64c9371e531694febee74255f67ccc88fe68c638
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-12-29 18:03:11 +0300

    added the save to firestore

--- EventGuru02/Fahad/PostViewController.swift
+++ EventGuru02/Fahad/PostViewController.swift
@@ -6,18 +6,49 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
 
 class PostViewController: UIViewController {
+    
+    var db: Firestore!
+    var uid = Auth.auth().currentUser?.uid
 
     @IBOutlet weak var textView: UITextView!
-    override func viewDidLoad() {
+    @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var price: UITextField!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
+    
+    var selectedCategory: String?
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         self.textView.layer.borderColor = UIColor.lightGray.cgColor
         self.textView.layer.borderWidth = 1
+        
+        db = Firestore.firestore()
     }
     
-
+    @IBAction func createEventBtn(_ sender: Any)
+    {
+        let eventData: [String: Any] =
+        [
+            "EventName": eventName.text ?? <#default value#>,
+            "Description": textView.text ?? <#default value#>,
+            "Price": price.text ?? <#default value#>,
+            "Location": location.text ?? <#default value#>,
+            "StartDate": startDatePicker.date,
+            "EndDate": endDatePicker.date,
+            "Category": selectedCategory ?? <#default value#>
+        ]
+        
+        db.collection("Events").document(uid ?? <#default value#>).setData(eventData)
+    }
+    
     /*
     // MARK: - Navigation
 
