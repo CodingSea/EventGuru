851342466062c0e623d2df6ea1e35bcf5e97cf7d
Shad-en <abdullahazhar20202@gmail.com>
2024-12-29 18:34:44 +0300

    Interest

---
+++ EventGuru02/Abdullah/InterestViewController.swift
@@ -0,0 +1,43 @@
+//
+//  InterestViewController.swift
+//  EventGuru02
+//
+//  Created by BP on 29/12/2024.
+//
+
+import UIKit
+
+
+class InterestViewController: UIViewController {
+    
+    @IBOutlet weak var Entertainmentlabel: UILabel!
+    @IBOutlet weak var SocialGatheringslabel: UILabel!
+    @IBOutlet weak var OutdoorActiviteslabel: UILabel!
+    @IBOutlet weak var PersonalDevelopmentlabel: UILabel!
+    @IBOutlet weak var Technologylabel: UILabel!
+    @IBOutlet weak var Fitnesslabel: UILabel!
+    @IBOutlet weak var Gaminglabel: UILabel!
+    @IBOutlet weak var Sportslabel: UILabel!
+    
+    @IBOutlet weak var EntertainmentSwitch: UISwitch!
+    @IBOutlet weak var SocialGatheringsSwitch: UISwitch!
+    @IBOutlet weak var OutDoorActivitiesSwitch: UISwitch!
+    @IBOutlet weak var PersonalDevelopmentSwitch: UISwitch!
+    @IBOutlet weak var TechnologySwitch: UISwitch!
+    @IBOutlet weak var FitnessSwitch: UISwitch!
+    @IBOutlet weak var GamingSwitch: UISwitch!
+    @IBOutlet weak var SportsSwitch: UISwitch!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
