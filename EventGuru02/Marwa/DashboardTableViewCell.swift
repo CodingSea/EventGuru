37e4700ed7ff9159b995171822252747600a2ec8
Mrwashz <marwashahzad107@gmail.com>
2024-12-31 01:35:56 +0300

    dashboard

---
+++ EventGuru02/Marwa/DashboardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardTableViewCell.swift
+//  EventGuru02
+//
+//  Created by Mac on 29/12/2024.
+//
+
+import UIKit
+
+class DashboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ EventGuru02/Marwa/DashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardViewController.swift
+//  EventGuru02
+//
+//  Created by Mac on 29/12/2024.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
